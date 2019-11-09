651eeb1b6054c87725efcaa45b84fa298cff044c
王治春 <wangzhichun001@ke.com>
2019-11-09 14:53:01 +0800

    配置loaders react等

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,16 @@
-console.log('---123---')
+import React from 'react'
+import ReactDom from 'react-dom'
+// import "./test.less"
+// import "./test.css"
+// import buyImg from "@/assets/img/icon_buy_task.png"
+// import testImg from "@/assets/img/bg_store.png"
+ class Home extends React.Component {
+    render(){
+        return (
+            <div className="test test2">
+                <p>hello world</p>
+            </div>
+        )
+    }
+}
+ReactDom.render(<Home/>,document.getElementById("app"))
